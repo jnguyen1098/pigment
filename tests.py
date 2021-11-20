b75d7d344f509fd3dc249f8305ba7063634deb57
jnguyen1098 <jnguyen1098@gmail.com>
2021-11-20 17:22:47 -0500

    test skeleton

---
+++ tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class PigmentTest(unittest.TestCase):
+    def test_lmao(cls):
+        cls.assertTrue(1)
+
+if __name__ == "__main__":
+    unittest.main()
