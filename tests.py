207f6c34acf80720e2b4120ad13f8626d4459722
jnguyen1098 <jnguyen1098@gmail.com>
2021-11-20 18:08:39 -0500

    back to lists

--- tests.py
+++ tests.py
@@ -21,17 +21,30 @@ class PigmentTest(unittest.TestCase):
         HIPPIE = "HIPPIES"
         ELAA = "ELAA"
         RETINOL = "RETINOL"
-        partitions = pigment.get_partitions(OrderedDict((
-            (BUFFET, [AHA, BHA, HIPPIE, ELAA, RETINOL]),
-            (AHA, [RETINOL]),
-            (BHA, [RETINOL]),
-            (HIPPIE, [AHA, BHA, RETINOL]),
-            (ELAA, [AHA, BHA, RETINOL]),
-        )))
+        partitions = pigment.get_partitions(OrderedDict([
+            [BUFFET, [AHA, BHA, HIPPIE, ELAA, RETINOL]],
+            [AHA, [RETINOL]],
+            [BHA, [RETINOL]],
+            [HIPPIE, [AHA, BHA, RETINOL]],
+            [ELAA, [AHA, BHA, RETINOL]],
+        ]))
         cls.assertEqual(partitions, [
             ["BUFFET"], ["AHA", "BHA"], ["HIPPIES", "ELAA"], ["RETINOL"]
         ])
 
+    def test_complete(cls):
+        A = "A"
+        B = "B"
+        C = "C"
+        D = "D"
+        E = "E"
+        partitions = pigment.get_partitions(OrderedDict([
+            [A, [B, C, D, E]],
+            [B, [C, D, E]],
+            [C, [D, E]],
+            [D, [E]],
+        ]))
+        cls.assertEqual(partitions, [[A], [B], [C], [D], [E]])
 
 if __name__ == "__main__":
     unittest.main()
