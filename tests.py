553b8afbcf0f7f3e9df46816d0c6ecafc00ae71c
jnguyen1098 <jnguyen1098@gmail.com>
2021-11-20 17:59:57 -0500

    boilerplate test

--- tests.py
+++ tests.py
@@ -6,16 +6,31 @@ from collections import OrderedDict
 
 class PigmentTest(unittest.TestCase):
 
-    @classmethod
-    def setUp(cls):
-        print("setup fixture")
-
     def test_simple(cls):
         partitions = pigment.get_partitions(OrderedDict((
             (1, (2, 3)),
             (2, (3, 4)),
         )))
         cls.assertEqual(partitions, [[1, 4], [2], [3]])
+
+
+    def test_boilerplate(cls):
+        BUFFET = "BUFFET"
+        AHA = "AHA"
+        BHA = "BHA"
+        HIPPIE = "HIPPIES"
+        ELAA = "ELAA"
+        RETINOL = "RETINOL"
+        partitions = pigment.get_partitions(OrderedDict((
+            (BUFFET, [AHA, BHA, HIPPIE, ELAA, RETINOL]),
+            (AHA, [RETINOL]),
+            (BHA, [RETINOL]),
+            (HIPPIE, [AHA, BHA, RETINOL]),
+            (ELAA, [AHA, BHA, RETINOL]),
+        )))
+        cls.assertEqual(partitions, [
+            ["BUFFET"], ["AHA", "BHA"], ["HIPPIES", "ELAA"], ["RETINOL"]
+        ])
 
 
 if __name__ == "__main__":
